65681969d6035b874659e97dd94a3008fabb727c
Andrew Butler <abutler3@gmail.com>
2014-07-23 16:12:18 -0500

    first commit

---
+++ menu.php
@@ -0,0 +1,537 @@
+<link type="text/css" rel="stylesheet" href="sdmenu/sdmenu.css" />
+<link type="text/css" rel="stylesheet" href="walkinclinics.css" />
+<script src="http://maps.google.com/maps/api/js?sensor=false"
+          type="text/javascript"></script>
+<script type="text/javascript" src="sdmenu/sdmenu.js"></script>
+
+<script type="text/javascript">
+	// <![CDATA[
+	var myMenu;
+	window.onload = function() {
+		myMenu = new SDMenu("walkinclinics");
+		myMenu.init();
+        myMenu.oneSmOnly = true;
+        myMenu.remember = false;
+	};
+	// ]]>
+</script>
+<style>
+    body {
+        margin:0;
+        width:100%;
+    }
+</style>
+<div style="width: 1134px; margin:0 auto;">
+<img src="top_mockup.jpg" style="border:0;">
+<br />
+
+
+<div style="float:left; width:270px; margin:10px 0 0 204px;" id="walkinclinics" class="sdmenu">
+
+<div class="collapsed">
+    <span><a href="#" onclick="showOnes();">FRANKLIN</a></span>
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Health and Williamson<br />Medical Center</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-791-7373">(615) 791-7373</a>
+    </p>
+
+    <a href="#"><img src="sdmenu/tell-us-btn.jpg" style="margin-left:42px; border:0;"></a>
+
+    <p class="home-steps-text">
+        919 Murfreesboro Road (Hwy. 96)
+        <br />
+        Franklin, TN 37064
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#franklinwalkin" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="50%">
+                Monday-Friday
+                <br />
+                7:30 a.m.-7:30 p.m.
+            </td>
+            <td width="50%">
+                Saturday-Sunday
+                <br />
+                8&nbsp;a.m.-5&nbsp;p.m.
+            </td>
+        </tr>
+    </table>
+
+    <br />
+    <hr />
+    <br />
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Health and Williamson Medical<br />Center Cool Springs Walk In Clinic</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-875-4200">(615) 875-4200</a>
+    </p>
+
+    <p class="home-steps-text">
+        1834 West McEwen Drive
+        <br />
+        Franklin, TN 37067
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#franklinwalkin" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="50%">
+                Monday-Friday
+                <br />
+                7:30 a.m.-7:30 p.m.
+            </td>
+            <td width="50%">
+                Saturday-Sunday
+                <br />
+                8&nbsp;a.m.-5&nbsp;p.m.
+            </td>
+        </tr>
+    </table>
+
+    <br />
+    <hr />
+    <br />
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Bone &amp; Joint After-Hours Clinic</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-261-0443">(615) 261-0443</a>
+    </p>
+
+    <p class="home-steps-text">
+        206 Bedford Way
+        <br />
+        Franklin, TN 37064
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#franklinwalkin" class="mapLink">map</a>
+    </p>
+
+    <p class="home-steps-text"
+        >Monday-Thursday
+        <br />
+        5&nbsp;p.m.-8&nbsp;p.m.
+    </p>
+</div>
+
+<div class="collapsed">
+    <span><a href="#" onclick="showFour();">HENDERSONVILLE</a></span>
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Children's After-Hours Clinic</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-822-5437">(615) 822-5437</a>
+    </p>
+
+    <p class="home-steps-text">
+       262 New Shackle Island Road,
+        <br />
+        Suite 207
+        <br />
+        Hendersonville, TN 37075
+        <br />
+        <a href="https://www.childrenshospital.vanderbilt.org/services.php?mid=10702" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="50%">
+                Monday-Friday
+                <br />
+                6-9:30 p.m.
+            </td>
+            <td width="50%">
+                Saturday-Sunday
+                <br />
+                12:30-7:30 p.m.
+            </td>
+        </tr>
+    </table>
+
+    <p class="home-steps-text">
+        No appointment is necessary,
+        <br />
+        but please call your pediatrician first.
+    </p>
+</div>
+
+<div class="collapsed">
+    <span><a href="#" onclick="showThree();">MT. JULIET</a></span>
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Children's After-Hours Clinic</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-754-7337">(615) 754-7337</a>
+    </p>
+
+    <p class="home-steps-text">
+        2025 North Mt. Juliet Road
+        <br />
+        Suite 200
+        <br />
+        Mt. Juliet, TN 37122
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#mtjulietwalkin" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="36%">
+                Monday-Friday
+                <br />
+                6-9:30 p.m.
+            </td>
+            <td width="31%">
+                Saturday
+                <br />
+                3-9:30 p.m.
+            </td>
+            <td width="33%">
+                Sunday
+                <br />
+                12-7:30 p.m.
+            </td>
+        </tr>
+    </table>
+
+    <p class="home-steps-text">
+        No appointment is necessary,
+        <br />
+        but please call your pediatrician first.
+    </p>
+</div>
+
+<div class="collapsed">
+    <span><a href="#" onclick="showTwos();">SPRING HILL</a></span>
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Health and Williamson<br />Medical Center</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-302-1111">(615) 302-1111</a>
+    </p>
+
+    <a href="#"><img src="sdmenu/tell-us-btn.jpg" style="margin-left:42px; border:0;"></a>
+
+    <p class="home-steps-text">
+        3098 Campbell Station Parkway
+        <br />
+        Spring Hill, TN 37174
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#springhillwalkin" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="50%">
+                Monday-Friday
+                <br />
+                7:30 a.m.-7:30 p.m.
+            </td>
+            <td width="50%">
+                Saturday-Sunday
+                <br />
+                8&nbsp;a.m.-5&nbsp;p.m.
+            </td>
+        </tr>
+    </table>
+
+    <br />
+    <hr />
+    <br />
+
+    <p class="home-steps-text textspace">
+        <strong class="title-text">Vanderbilt Children's After-Hours Clinic</strong>
+        <br />
+        <!--<a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43216">About Us</a>-->
+        <a href="tel:615-380-0448">(931) 380-0448</a>
+    </p>
+
+    <p class="home-steps-text">
+        1003 Reserve Blvd.
+        <br />
+        Suite 110
+        <br />
+        Spring Hill, TN 37174
+        <br />
+        <a href="http://www.mc.vanderbilt.edu/root/vumc.php?site=walkinclinics&amp;doc=43218#springhillwalkin" class="mapLink">map</a>
+    </p>
+
+    <table width="100%">
+        <tr>
+            <td width="50%">
+                Monday-Friday
+                <br />
+                6-9:30 p.m.
+            </td>
+            <td width="50%">
+                Saturday-Sunday
+                <br />
+                12:30-7:30 p.m.
+            </td>
+        </tr>
+    </table>
+
+    <br />
+
+</div>
+
+</div>
+<div>
+    <div class="map" style="float: left; margin:14px 0 0 20px; border:1px solid #cecece; border-radius:10px; -webkit-border-radius:10px; -moz-border-radius:10px;">
+      <!--<input onclick="showOnes();" type=button value="Show Franklin Clinics">
+      <input onclick="showTwos();" type=button value="Show Spring Hill Clinics">
+      <input onclick="showThree();" type=button value="Show Mt. Juliet Clinics">
+      <input onclick="showFour();" type=button value="Show Hendersonville Clinics">
+  <br>
+  <br>-->
+  <div id="map" style="width: 588px; height: 600px; border-radius:10px; -webkit-border-radius:10px; -moz-border-radius:10px;"></div>
+    </div>
+  <script type="text/javascript">
+    var map;
+	 var locations = [];
+
+    function initialize() {
+   		 var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Children\'s Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+    }
+
+
+    function showOnes() {
+      var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Childrens Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+	  var franklin = locations.splice(0,3);
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < franklin.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(franklin[i][1], franklin[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+    }
+
+
+    function showTwos() {
+          var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Children\'s Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+      locations.splice(0, 3);
+		 var spring = locations.splice(0,2);
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < spring.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(spring[i][1], spring[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+      /* alert(locations);
+      google.maps.event.addDomListener(window, 'load', initialize); */
+    }
+
+    function showTwos() {
+          var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Childrens Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+      locations.splice(0, 3);
+		 var spring = locations.splice(0,2);
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < spring.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(spring[i][1], spring[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    /*  alert(locations);
+      google.maps.event.addDomListener(window, 'load', initialize); */
+    }
+
+    function showThree() {
+    	var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Childrens Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+    locations.splice(0, 4);
+		 var mount = locations.splice(1,1);
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < mount.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(mount[i][1], mount[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    /* alert(locations);
+    alert(mount);
+    google.maps.event.addDomListener(window, 'load', initialize); */
+    }
+
+      function showFour() {
+    	var locations = [
+      ['Williamson Medical Center', 35.917367, -86.841134, 1],
+      ['Cool Springs Walk In', 35.935867, -86.825720, 1],
+      ['Bone and Joint', 35.906508, -86.838920, 1],
+      ['Williamson Medical Center', 35.774426, -86.910618, 2],
+      ['Childrens Walkin', 35.736806, -86.897706, 2],
+      ['Mt. Juliet', 36.197899, -86.517850, 3],
+      ['Hendersonville', 36.322244, -86.620636, 4]
+    ];
+		 locations.splice(0,6);
+      var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(36.025, -86.668),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    /* alert(locations);
+    google.maps.event.addDomListener(window, 'load', initialize); */
+    }
+
+   google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
+</div>
+</div>
